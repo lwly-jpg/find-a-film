5963ffe6ebdb7158304e5466386a8953eb6a4281
Joseph Lander <josephclander@gmail.com>
2022-12-19 18:00:47 +0000

    Add nav, header, search

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-// import './App.css';
+import React from "react";
+import typeLogo from "./images/typeface-based-logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
+    <>
+      <nav className="nav">
+        <div className="nav__header">
+          <img className="nav__logo" src={typeLogo} alt="logo" />
+        </div>
+      </nav>
+      <header className="header">
+        <h1 className="headline">
+          <span className="discover">Discover</span> your next watch
+        </h1>
       </header>
-    </div>
+      <form action="#" className="searchbar">
+        <input
+          type="text"
+          name="search"
+          placeholder="find your film"
+          className="searchbar__input"
+        />
+        <input type="submit" value="Search" className="searchbar__submit" />
+      </form>
+    </>
   );
 }
 
