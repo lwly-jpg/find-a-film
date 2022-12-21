b5e0ddde5bc4705c896817ad418bcc0308551334
adonl <adnlwly@gmail.com>
2022-12-21 15:56:32 +0000

    set up film data endpoint fetch

--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -1,7 +1,17 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import apiKey from '../apiKey';
+
 
 const Film = () => {
   const { film_id } = useParams();
+  const [filmData, setFilmData] = useState();
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${film_id}?api_key=${apiKey}&language=en-GB}`)
+      .then(response => response.json())
+        .then(data => console.log(data))
+  });
 
   return <div>FILM number {film_id}</div>;
 };
