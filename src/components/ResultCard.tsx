400df9e4e5c14c25737239ded289139d0916d105
Iain <iain.hoolahan@gmail.com>
2022-12-21 17:42:51 +0000

    genre object added - WIP

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -9,11 +9,12 @@ const ResultCard = (result: any) => {
     <div className='result'>
       <img className='result__image' src={mock_1} alt='' />
       <div className='result__info'>
-        <div className='result__header'>{result}</div>
+        <div className='result__header'>{result.genre_ids.map((item: number) => (genre[item]: any)) }</div>
+        <div className='result__header'>{genre[27]}</div>
         <h3 className='result__title'>{result.title}</h3>
-        <div className='result__minor-info'>1984 - PG - 1h45m</div>
-        <div className='result__description'>
-          {result.overview}
+        <div className='result__minor-info'>{result.release_date.split("-")[0]} - PG - 1h45m</div>
+          <div className='result__description'>
+          {result.overview.substring(0,230)}...
         </div>
       </div>
     </div>
@@ -23,3 +24,27 @@ const ResultCard = (result: any) => {
 }
 
 export default ResultCard;
+
+// .findIndex(x => x.id === item)}}
+
+const genre = { 
+  28: 'Action', 
+  12: 'Adventure',
+  16: 'Animation',
+  35: 'Comedy',
+  80: 'Crime',
+  99: 'Documentary',
+  18: 'Drama',
+  10751: 'Family',
+  14: 'Fantasy',
+  36: 'History',
+  27: 'Horror',
+  10402: 'Music',
+  9648: 'Mystery',
+  10749: 'Romance',
+  878: 'Science Fiction',
+  10770: 'TV Movie',
+  53: 'Thriller',
+  10752: 'War',
+  37: 'Western'
+}
