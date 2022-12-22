c31f5a9233e38143e8dfadac8ea96157144a79c3
Joseph Lander <josephclander@gmail.com>
2022-12-22 08:44:56 +0000

    Add new styles

--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -1,28 +1,32 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import "./Film.css"
+import './Film.css';
 import star from '../images/star.png';
-import mockData from '../mocks/singleFilmDataMock'
+import netflix from '../images/netflix-icon.png'
+import prime from '../images/prime-icon.png'
+import mockData from '../mocks/singleFilmDataMock';
 
 const getPosterURL = (posterpath: string) => {
   return `https://www.themoviedb.org/t/p/w220_and_h330_face${posterpath}`;
 };
 
-const Film = (film: any) => {
+const Film = () => {
   // film_id from the url
   const { film_id } = useParams();
 
   //TODO Will you have to pass info through?
   // or can get it all from the film_id?
-  const [filmData, setFilmData] = useState(mockData);
-  
+  const [filmData] = useState(mockData);
+
+  //TODO Type for this
   const genreList = filmData.genre_ids.map((item) => genre[item]).join(', ');
 
   // useEffect here?
 
   return (
     <div className='film__container'>
-      <div>FILM number {film_id}</div>
+      <div>film_id: {film_id}</div>
+      <h1 className='film__title'>{filmData.title}</h1>
       <div className='film'>
         <img
           className='film__image'
@@ -30,24 +34,28 @@ const Film = (film: any) => {
           alt=''
         />
         <div className='film__info'>
-          <div className='film__header'>
-            <div className='film__header--rating'>
-              <img className='rating' src={star} alt='' />
-              <div className='score'>{filmData.vote_average} / 10</div>
-            </div>
-            <div className='film__header--imdb'>
-              <a href='https://www.imdb.com/'>View on IMDB</a>
-            </div>
-          </div>
-          <h1 className='film__title'>{filmData.title}</h1>
-          <div className='film__minor-info'>
-            Released: {filmData.release_date.split('-')[0]}
-          </div>
           <div className='film__header--genre'>{genreList}</div>
-          <div className='film__description'>
-            {filmData.overview.substring(0, 230)}...
+          <div className='film__header--rating'>
+            <img className='rating' src={star} alt='' />
+            <div className='score'>{filmData.vote_average} / 10</div>
+          </div>
+          <div className='film__header--imdb'>
+            <a href='https://www.imdb.com/'>View on IMDB</a>
+          </div>
+          <div className='film__minor-info'>
+            <span className='helper__blue'>Released:</span>{' '}
+            {filmData.release_date.split('-')[0]}
+          </div>
+          <div className="film__minor-info"><span className="helper__blue">Runtime: </span>X hr XX mins</div>
+          <div className="film__minor-info"><span className="helper__blue">Watch on: </span></div>
+          <div className="film__providers">
+            <img src={netflix} alt="" />
+            <img src={prime} alt="" />
           </div>
         </div>
+      </div>
+      <div className='film__description'>
+        {filmData.overview}
       </div>
     </div>
   );
--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -5,8 +5,6 @@ const getPosterURL = (posterpath: string) => {
 };
 
 const ResultCard = (result: any) => {
-  const genreList = result.genre_ids.map((item) => genre[item]).join(', ');
-
   return (
     <div className='results__container'>
       <div className='result'>
@@ -27,9 +25,8 @@ const ResultCard = (result: any) => {
           </div>
           <h1 className='result__title'>{result.title}</h1>
           <div className='result__minor-info'>
-            Released: {result.release_date.split('-')[0]}
+            <span className="helper__blue">Released:</span> {result.release_date.split('-')[0]}
           </div>
-          <div className='result__header--genre'>{genreList}</div>
           <div className='result__description'>
             {result.overview.substring(0, 230)}...
           </div>
@@ -40,25 +37,3 @@ const ResultCard = (result: any) => {
 };
 
 export default ResultCard;
-
-const genre = {
-  28: 'Action',
-  12: 'Adventure',
-  16: 'Animation',
-  35: 'Comedy',
-  80: 'Crime',
-  99: 'Documentary',
-  18: 'Drama',
-  10751: 'Family',
-  14: 'Fantasy',
-  36: 'History',
-  27: 'Horror',
-  10402: 'Music',
-  9648: 'Mystery',
-  10749: 'Romance',
-  878: 'Science Fiction',
-  10770: 'TV Movie',
-  53: 'Thriller',
-  10752: 'War',
-  37: 'Western',
-};
