a43657f19e37f7187d3f191e12f04d92f1f623e3
adonl <adnlwly@gmail.com>
2022-12-23 09:21:49 +0000

    add cleanup function to GET watchProviders useEffect

--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -37,18 +37,26 @@ const Film = () => {
       return () => {
         cancelled = true;
       }
-      
+
   }, [film_id]);
 
   // GET watchProviders data
   useEffect(() => {
+    let cancelled = false;
     fetch(
       `https://api.themoviedb.org/3/movie/${film_id}/watch/providers?api_key=${apiKey}&language=en-GB`
     )
       .then((response) => response.json())
       .then(async (data) => {
-        setWatchProviders(data.results.GB); // .GB === country
+        if (!cancelled) {
+          setWatchProviders(data.results.GB); // .GB === country
+        }
       });
+
+      return () => {
+        cancelled = true;
+      }
+      
   }, [film_id]);
 
   return (
