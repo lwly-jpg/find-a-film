4a2c7ccf8448c0111c540207309c300e25d4d548
adonl <adnlwly@gmail.com>
2022-12-27 18:41:45 +0000

    add all remaining genres

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -124,6 +124,22 @@ const SearchResult = () => {
           <option value={28}>Action</option>
           <option value={12}>Adventure</option>
           <option value={16}>Animation</option>
+          <option value={35}>Comedy</option>
+          <option value={80}>Crime</option>
+          <option value={99}>Documentary</option>
+          <option value={18}>Drama</option>
+          <option value={10751}>Family</option>
+          <option value={14}>Fantasy</option>
+          <option value={36}>History</option>
+          <option value={27}>Horror</option>
+          <option value={10402}>Music</option>
+          <option value={9648}>Mystery</option>
+          <option value={10749}>Romance</option>
+          <option value={878}>Science Fiction</option>
+          <option value={10770}>TV Movie</option>
+          <option value={53}>Thriller</option>
+          <option value={10752}>War</option>
+          <option value={37}>Western</option>
         </select>
         <button className='searchbar__submit'>Discover films</button>
       </form>
@@ -168,3 +184,8 @@ export default SearchResult;
 
 const { format } = require('date-fns');
 let date = format(new Date(), 'yyyy.MM.dd');
+
+
+
+
+
