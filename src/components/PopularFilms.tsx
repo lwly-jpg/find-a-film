b462ef0ae4bf15e688da5f320de2cedd74b21559
Joseph Lander <josephclander@gmail.com>
2022-12-30 17:22:43 +0000

    Add useEffect & max size of carousel

--- src/components/PopularFilms.tsx
+++ src/components/PopularFilms.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
-import apiKey from '../apiKey';
-import '../components/SearchResult.css'
-import SimilarFilmCard from './SimilarFilmCard';
-
+import { useEffect, useState } from "react";
+import apiKey from "../apiKey";
+import "../components/SearchResult.css";
+import SimilarFilmCard from "./SimilarFilmCard";
+const { format } = require("date-fns");
+let date = format(new Date(), "yyyy.MM.dd");
 
 const PopularFilms = () => {
   const [popularResults, setPopularResults] = useState([]);
-  const [msg, setMsg] = useState('');
+  const [msg, setMsg] = useState("");
 
-  const getPopular = () => {
-    
+  useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
     )
@@ -19,7 +19,7 @@ const PopularFilms = () => {
         // need empty array to avoid errors
         const newResults = data.results || [];
         if (data.results.length === 0) {
-          setMsg('Sorry no results');
+          setMsg("Sorry no results");
           setPopularResults([]);
         }
         if (data.results.length > 0) {
@@ -30,28 +30,25 @@ const PopularFilms = () => {
       .catch((err) => {
         console.log(err);
       });
-  };
- 
-  getPopular();
+  }, []);
 
   return (
-    <div className='similar__films'>
-      <h3 className='similar__films__header'>{msg}</h3>
-      <div className='film_carousel'>
-        {popularResults.filter((item: any) => {
-          if (item.poster_path !== null && item.release_date < date ) {
-            return item
-          } return null
-        })
-        .map((result: any) => (
-          <SimilarFilmCard key={result.id} {...result} />
-        ))}
+    <div className="similar__films">
+      <h3 className="similar__films__header">{msg}</h3>
+      <div className="film_carousel">
+        {popularResults
+          .filter((item: any) => {
+            if (item.poster_path !== null && item.release_date < date) {
+              return item;
+            }
+            return null;
+          })
+          .map((result: any) => (
+            <SimilarFilmCard key={result.id} {...result} />
+          ))}
       </div>
     </div>
   );
 };
-  
-export default PopularFilms;
 
-const { format } = require('date-fns');
-let date = format(new Date(), 'yyyy.MM.dd');
+export default PopularFilms;
