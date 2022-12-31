f09e3662f3208e3e5202a700fd66d3d1dbf7ac7f
Alastair <alastair.chau@gmail.com>
2022-12-31 14:19:38 +0000

    Cypress test: No result and empty search passing

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -3,11 +3,24 @@ describe('Search function', () => {
     cy.visit('http://localhost:3000')
   })
   
-  it("gets Avatar results", () => {
+  it("displays result header", () => {
     cy.get(".searchbar").type("avatar")
     cy.get("[class='searchbar__submit']").click();
 
-    cy.contains("Results for avatar")
+    cy.contains("[class='results__message']","Results for avatar")
   })
 
+  it("displays no result header for search yielding no results", () => {
+    cy.get(".searchbar").type("]")
+    cy.get("[class='searchbar__submit']").click();
+
+    cy.contains("[class='results__message']","Sorry no results")
+  })
+
+  it("displays custom message when search contains no user input", () => {
+    cy.get(".searchbar")
+    cy.get("[class='searchbar__submit']").click();
+
+    cy.contains("[class='results__message']","Must input a search")
+  })
 })
