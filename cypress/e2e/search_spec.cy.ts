e2de55e49ab4d3e7663f7f164bbc0894eea9bb11
Alastair <alastair.chau@gmail.com>
2022-12-31 17:16:06 +0000

    Cypress test passing: displays only the relevant sort buttons after filtering search results

---
+++ cypress/e2e/search_spec.cy.ts
@@ -0,0 +1,77 @@
+describe('Conducting a search', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+  
+  it("displays result header", () => {
+    cy.get(".searchbar").type("avatar")
+    cy.get(".searchbar__submit").click();
+
+    cy.contains(".results__message","Results for avatar")
+  })
+
+  it("displays no result header for search yielding no results", () => {
+    cy.get(".searchbar").type("]")
+    cy.get(".searchbar__submit").click();
+
+    cy.contains(".results__message","Sorry no results")
+  })
+
+  it("displays custom message when search contains no user input", () => {
+    cy.get(".searchbar")
+    cy.get(".searchbar__submit").click();
+
+    cy.contains(".results__message","Must input a search")
+  })
+
+  it("displays result sort label and buttons", () => {
+  cy.get(".searchbar").type("terminator")
+  cy.get(".searchbar__submit").click();
+
+  cy.get(".sort__options").contains("Sort by:")
+  cy.get(".sort__options").find("button.sort__button").should('have.length',2)  
+  })
+
+  it("does not display result container for invalid search", () => {
+    cy.get(".searchbar").type("'")
+    cy.get(".searchbar__submit").click();
+  
+    cy.get(".results__container").should('be.empty')
+  })
+
+  it("does not display result container for empty search", () => {
+    cy.get(".searchbar")
+    cy.get(".searchbar__submit").click();
+  
+    cy.get(".results__container").should('be.empty')
+  })
+
+  it("after initial search, it shows release year and rating sort buttons", () => {
+    cy.get(".searchbar").type("michael clayton")
+    cy.get(".searchbar__submit").click();
+  
+    cy.get(".sort__options").find('button').contains('Release year')
+    cy.get(".sort__options").find('button').contains("Rating")
+    cy.get(".sort__options").find('button').contains("Relevance").should('not.exist')
+  })
+
+  it("after filtering results by release year, only shows rating and relevance sort buttons", () => {
+    cy.get(".searchbar").type("heat")
+    cy.get(".searchbar__submit").click();
+    cy.get(".sort__options").contains('Release year').click()
+
+    cy.get(".sort__options").find('button').contains('Relevance')
+    cy.get(".sort__options").find('button').contains("Rating")
+    cy.get(".sort__options").find('button').contains("Release year").should('not.exist')
+  })
+
+  it("after filtering results by rating, only shows release year and relevance sort buttons", () => {
+    cy.get(".searchbar").type("heat")
+    cy.get(".searchbar__submit").click();
+    cy.get(".sort__options").contains('Rating').click()
+
+    cy.get(".sort__options").find('button').contains('Relevance')
+    cy.get(".sort__options").find('button').contains("Release year")
+    cy.get(".sort__options").find('button').contains("Rating").should('not.exist')
+  })
+})
